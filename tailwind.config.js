40fab5b30be0f88b6d007ac97cd053dd4837b4ae
Bob VanLonkhuyzen <hey.bob.oh@gmail.com>
2023-07-04 16:01:29 -0400

    Add custom gray colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  important: true,
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -11,6 +12,19 @@ module.exports = {
       'md': '768px',
       'lg': '1024px',
       'xl': '1280px',
+    },
+    colors: {
+        gray: {
+          '100': '#d0ccd0',
+          '200': '#b9b5b9',
+          '300': '#a2a0a2',
+          '400': '#8c8a8c',
+          '500': '#777577',
+          '600': '#626162',
+          '700': '#4e4d4e',
+          '800': '#3b3b3b',
+          '900': '#292929',
+        }
     },
     extend: {
       backgroundImage: {
