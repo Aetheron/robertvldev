fe7e5ed715ffcb68150fac9145ca3b9f694a2822
Bob VanLonkhuyzen <hey.bob.oh@gmail.com>
2026-02-26 14:17:35 -0500

    Upgrade next to 14 and node to 22

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,14 @@
-"use client"
 import type { NextRequest } from "next/server"
 import { NextResponse } from "next/server"
 
 export function middleware(request: NextRequest) {
   const url = request.nextUrl.clone()
   if (url.pathname === "/portfolio/sparrow-health") {
-    url.pathname = "/portfolio/php"
+    url.pathname = "/portfolio/uofmhealthplan"
+    return NextResponse.redirect(url)
+  }
+  if (url.pathname === "/portfolio/php") {
+    url.pathname = "/portfolio/uofmhealthplan"
     return NextResponse.redirect(url)
   }
 }
