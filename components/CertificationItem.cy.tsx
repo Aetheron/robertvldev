9e6ec246226ca1a0e4d6e7f98c0a4d737aad25dd
Bob VanLonkhuyzen <hey.bob.oh@gmail.com>
2026-03-02 14:54:29 -0500

    Fix component tests

--- components/CertificationItem.cy.tsx
+++ components/CertificationItem.cy.tsx
@@ -1,17 +1,24 @@
-import React from "react"
 import CertificationItem from "./CertificationItem"
 
-const date = new Date().getDate().toString()
+const date = new Date().getDate()
 
 describe("<CertificationItem />", () => {
   it("renders", () => {
     cy.mount(
-      <CertificationItem title="Cypress Test" date={date} issuer="Cypress.io" />
+      <CertificationItem
+        title="Cypress Test"
+        dateIssued={date}
+        issuer="Cypress.io"
+      />
     )
   })
   it("has the correct text", () => {
     cy.mount(
-      <CertificationItem title="Cypress Test" date={date} issuer="Cypress.io" />
+      <CertificationItem
+        title="Cypress Test"
+        dateIssued={date}
+        issuer="Cypress.io"
+      />
     )
     cy.get("div.p-card-title").should("have.text", "Cypress Test")
     cy.get("div.p-card-content").should("contain", date)
--- components/CourseItem.cy.tsx
+++ components/CourseItem.cy.tsx
@@ -1,17 +1,16 @@
-import React from "react"
 import CourseItem from "./CourseItem"
 
-const date = new Date().getDate().toString()
+const date = new Date().getDate()
 
 describe("<CourseItem />", () => {
   it("renders", () => {
     cy.mount(
-      <CourseItem title="Cypress Test" date={date} issuer="Cypress.io" />
+      <CourseItem title="Cypress Test" dateIssued={date} issuer="Cypress.io" />
     )
   })
   it("has the correct text", () => {
     cy.mount(
-      <CourseItem title="Cypress Test" date={date} issuer="Cypress.io" />
+      <CourseItem title="Cypress Test" dateIssued={date} issuer="Cypress.io" />
     )
     cy.get("div.p-card-title").should("have.text", "Cypress Test")
     cy.get("div.p-card-content").should("contain", date)
