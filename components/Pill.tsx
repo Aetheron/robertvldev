4c7684657774b71836dda69e5660ec812a93b5b7
Bob VanLonkhuyzen <hey.bob.oh@gmail.com>
2026-03-04 11:54:47 -0500

    Install twmerge and use in Pill component

--- components/Pill.tsx
+++ components/Pill.tsx
@@ -1,5 +1,5 @@
-import clsx from "clsx"
 import { HTMLAttributes } from "react"
+import { twMerge } from "tailwind-merge"
 
 export default function Pill({
   className,
@@ -8,9 +8,9 @@ export default function Pill({
   return (
     <span
       className={
-        `inline-flex items-center rounded-full` +
-        clsx(
-          " bg-indigo-100 px-4 py-2 text-xs font-medium text-indigo-700 dark:bg-indigo-400/10 dark:text-indigo-400",
+        `inline-flex items-center rounded-full ` +
+        twMerge(
+          "bg-indigo-100 px-4 py-2 text-xs font-medium text-indigo-700 dark:bg-indigo-400/10 dark:text-indigo-400",
           className
         )
       }
