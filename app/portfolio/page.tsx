345063154d3e234b6b33e1cbbd383029f13be46e
Bob VanLonkhuyzen <hey.bob.oh@gmail.com>
2026-03-16 11:09:45 -0400

    Make portfolio page prettier with image cards

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,6 +1,13 @@
-import Button from "@/components/Button"
-import Card from "@/components/Card"
-import Link from "next/link"
+import PortfolioListItem, {
+  PortfolioImageCardType,
+} from "@/components/PortfolioImageCard"
+import csw from "@/public/csw/hero.svg"
+import jets from "@/public/jets/hero.svg"
+import lippincott from "@/public/lippincott/hero.jpg"
+import pwp from "@/public/pwp/hero.svg"
+import spax from "@/public/spax/hero.svg"
+import stockx from "@/public/stockx/hero.svg"
+import umhp from "@/public/umhp/hero.svg"
 import { Metadata } from "next/types"
 
 export const metadata: Metadata = {
@@ -8,90 +15,65 @@ export const metadata: Metadata = {
 }
 
 export default function Portfolio() {
+  const links: PortfolioImageCardType[] = [
+    {
+      title: "Automated Deployments",
+      type: "Project",
+      link: "/portfolio/automated-deployments",
+      image: null,
+    },
+    {
+      title: "University of Michigan Health Plan",
+      type: "Client",
+      link: "/portfolio/uofmhealthplan",
+      image: umhp,
+    },
+    {
+      title: "SPAX",
+      type: "Client",
+      link: "/portfolio/spax",
+      image: spax,
+    },
+    {
+      title: "StockX",
+      type: "Client",
+      link: "/portfolio/stockx",
+      image: stockx,
+    },
+    {
+      title: "Jet's Pizza",
+      type: "Client",
+      link: "/portfolio/jets-pizza",
+      image: jets,
+    },
+    {
+      title: "Lippincott",
+      type: "Client",
+      link: "/portfolio/lippincott",
+      image: lippincott,
+    },
+    {
+      title: "CSW",
+      type: "Client",
+      link: "/portfolio/csw",
+      image: csw,
+    },
+    {
+      title: "Preaching and Worship",
+      type: "Project",
+      link: "/portfolio/pwp",
+      image: pwp,
+    },
+  ]
+
   return (
     <div>
       <h1>Portfolio</h1>
       <div className="flex flex-wrap gap-3 mt-4">
-        <Card
-          className="w-full bg-gray-800 shadow-md lg:min-w-[250px] lg:w-auto hover:shadow-xl hover:transition-all"
-          cardTitle="Automated Deployments"
-        >
-          <Link href="/portfolio/automated-deployments">
-            <Button className="bg-orange bg-opacity-10 cursor-pointer hover:bg-orange hover:text-white hover:opacity-100 dark:bg-orange dark:hover:bg-orange-400">
-              Read more
-            </Button>
-          </Link>
-        </Card>
-        <Card
-          className="w-full bg-gray-800 shadow-md lg:min-w-[250px] lg:w-auto hover:shadow-xl hover:transition-all"
-          cardTitle="U of M Health Plan"
-        >
-          <Link href="/portfolio/uofmhealthplan">
-            <Button className="bg-orange bg-opacity-10 cursor-pointer hover:bg-orange hover:text-white hover:opacity-100 dark:bg-orange dark:hover:bg-orange-400">
-              Read more
-            </Button>
-          </Link>
-        </Card>
-        <Card
-          className="w-full bg-gray-800 shadow-md lg:min-w-[250px] lg:w-auto hover:shadow-xl hover:transition-all"
-          cardTitle="SPAX"
-        >
-          <Link href="/portfolio/spax">
-            <Button className="bg-orange bg-opacity-10 cursor-pointer hover:bg-orange hover:text-white hover:opacity-100 dark:bg-orange dark:hover:bg-orange-400">
-              Read more
-            </Button>
-          </Link>
-        </Card>
-        <Card
-          className="w-full bg-gray-800 shadow-md lg:min-w-[250px] lg:w-auto hover:shadow-xl hover:transition-all"
-          cardTitle="StockX"
-        >
-          <Link href="/portfolio/stockx">
-            <Button className="bg-orange bg-opacity-10 cursor-pointer hover:bg-orange hover:text-white hover:opacity-100 dark:bg-orange dark:hover:bg-orange-400">
-              Read more
-            </Button>
-          </Link>
-        </Card>
-        <Card
-          className="w-full bg-gray-800 lg:min-w-[250px] lg:w-auto shadow-md hover:shadow-xl hover:transition-all"
-          cardTitle="Jet's Pizza"
-        >
-          <Link href="/portfolio/jets-pizza">
-            <Button className="bg-orange bg-opacity-10 cursor-pointer hover:bg-orange hover:text-white hover:opacity-100 dark:bg-orange dark:hover:bg-orange-400">
-              Read more
-            </Button>
-          </Link>
-        </Card>
-        <Card
-          className="w-full bg-gray-800 lg:min-w-[250px] lg:w-auto shadow-md hover:shadow-xl hover:transition-all"
-          cardTitle="Lippincott"
-        >
-          <Link href="/portfolio/lippincott">
-            <Button className="bg-orange bg-opacity-10 cursor-pointer hover:bg-orange hover:text-white hover:opacity-100 dark:bg-orange dark:hover:bg-orange-400">
-              Read more
-            </Button>
-          </Link>
-        </Card>
-        <Card
-          className="w-full bg-gray-800 lg:min-w-[250px] lg:w-auto shadow-md hover:shadow-xl hover:transition-all"
-          cardTitle="CSW"
-        >
-          <Link href="/portfolio/csw">
-            <Button className="bg-orange bg-opacity-10 cursor-pointer hover:bg-orange hover:text-white hover:opacity-100 dark:bg-orange dark:hover:bg-orange-400">
-              Read more
-            </Button>
-          </Link>
-        </Card>
-        <Card
-          className="w-full bg-gray-800 lg:min-w-[250px] lg:w-auto shadow-md hover:shadow-xl hover:transition-all"
-          cardTitle="Preaching and Worship"
-        >
-          <Link href="/portfolio/pwp">
-            <Button className="bg-orange bg-opacity-10 cursor-pointer hover:bg-orange hover:text-white hover:opacity-100 dark:bg-orange dark:hover:bg-orange-400">
-              Read more
-            </Button>
-          </Link>
-        </Card>
+        {links &&
+          links.map((link) => (
+            <PortfolioListItem key={link.title} item={link} />
+          ))}
       </div>
     </div>
   )
---
+++ components/PortfolioImageCard.tsx
@@ -0,0 +1,39 @@
+import Image, { StaticImageData } from "next/image"
+import Pill from "./Pill"
+
+export type PortfolioImageCardType = {
+  title: string
+  type: string
+  link: string
+  image: StaticImageData
+}
+
+export default function PortfolioLImageCard({
+  item,
+}: {
+  item: PortfolioImageCardType
+}) {
+  return (
+    <div className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-gray-900 min-w-1/6 px-8 pt-80 pb-8 sm:pt-48 dark:bg-gray-800">
+      <Image
+        alt=""
+        src={item.image}
+        className="absolute inset-0 -z-10 size-full object-cover"
+      />
+      <div className="absolute inset-0 -z-10 bg-linear-to-t from-gray-900 via-gray-900/40 dark:from-black/80 dark:via-black/40" />
+      <div className="absolute inset-0 -z-10 rounded-2xl inset-ring inset-ring-gray-900/10 dark:inset-ring-white/10" />
+
+      <div className="flex flex-wrap items-center gap-y-1 overflow-hidden text-sm/6 text-gray-300">
+        <Pill className="text-orange-400 dark:text-orange-500 bg-gray-100 dark:bg-gray-400/20">
+          {item.type}
+        </Pill>
+      </div>
+      <h3 className="mt-3 text-lg/6 font-semibold text-gray-50">
+        <a href={item.link}>
+          <span className="absolute inset-0" />
+          {item.title}
+        </a>
+      </h3>
+    </div>
+  )
+}
